f4e988a6bb2b811b1e1b5c20dee0e367261698ad
BitBadCoin <112504026+BitBadCoin@users.noreply.github.com>
2024-10-05 22:29:23 +0300

    y

---
+++ app/api/db/connection/page.tsx
@@ -0,0 +1,33 @@
+import { Sequelize } from '@sequelize/core';
+import { PostgresDialect } from '@sequelize/postgres';
+
+const sequelize = new Sequelize({
+    dialect: PostgresDialect,
+    database: 'hydra',
+    user: 'ortem',
+    password: 'p@ss4@rTeM!(*%',
+    host: 'localhost',
+    port: 5432,
+    ssl: true,
+    clientMinMessages: 'notice',
+  });
+
+export default function Page() {
+
+    try {
+        await sequelize.authenticate();
+    } catch (error) {
+        return (<h5>Connection authenticate error: {error}</h5>);
+    }
+
+    try {
+        await sequelize.close();
+    } catch (error) {
+        return (<h5>Connection close error: {error}</h5>);
+    }
+
+    return (
+        <h5>Connection check successfull.</h5>
+    )
+
+}
