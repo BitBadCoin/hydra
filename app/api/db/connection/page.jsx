e438601fc98b2cc6222c9cf36371f03994cd16b4
BitBadCoin <112504026+BitBadCoin@users.noreply.github.com>
2024-10-06 15:09:33 +0300

    y

--- app/api/db/connection/page.jsx
+++ app/api/db/connection/page.jsx
@@ -7,7 +7,6 @@ const sequelize = new Sequelize(
     'p@ss4@rTeM!(*%', {
         host: 'localhost',
         dialect: 'postgres',
-        dialectModule: pg,
   });
 
 export default async function Page() {
@@ -23,5 +22,4 @@ export default async function Page() {
     return (
         <h5>Connection check successfull.</h5>
     );
-
 }
