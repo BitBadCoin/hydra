ca453d2757faef715b4488b007b5b8b715fe24e2
BitBadCoin <112504026+BitBadCoin@users.noreply.github.com>
2024-10-06 12:04:53 +0300

    y

--- test_db.jsx
+++ test_db.jsx
@@ -13,16 +13,16 @@ async function Page() {
     try {
         await sequelize.authenticate();
     } catch (error) {
-        console.log(<h5>Connection authenticate error: {error}</h5>);
+        console.log(`<h5>Connection authenticate error: ${error}</h5>`);
     }
 
     try {
         await sequelize.close();
     } catch (error) {
-        console.log(<h5>Connection close error: {error}</h5>);
+        console.log(`<h5>Connection close error: ${error}</h5>`);
     }
 
-    console.log(<h5>Connection check successfull.</h5>);
+    console.log(`<h5>Connection check successfull.</h5>`);
 
 }
 
