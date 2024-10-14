eeae04585f1bbaa88aa7bb8fa18ee5b921137c60
BitBadCoin <112504026+BitBadCoin@users.noreply.github.com>
2024-10-14 18:34:46 +0300

    y

--- db/sequelize.js
+++ db/sequelize.js
@@ -2,7 +2,8 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize('hydra', 'ortem', 'p@$$4@rtem2024', {
     host: 'localhost',
-    dialect: 'postgres' /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
+    dialect: 'postgres', /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
+    dialectModule: pg
   });
 
 module.exports = sequelize
